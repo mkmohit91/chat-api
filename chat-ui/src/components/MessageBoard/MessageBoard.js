3cb71f0715b6bd8cfb4976ad81d9e705ce558b6c
Mohit Kumar <mkmohities7@gmail.com>
2021-01-24 20:22:31 +0000

    Added not empty validations in React

--- chat-ui/src/components/MessageBoard/MessageBoard.js
+++ chat-ui/src/components/MessageBoard/MessageBoard.js
@@ -1,17 +1,40 @@
 import styles from './MessageBoard.module.css';
 import {useState} from 'react'
 import {postMessage} from '../../services/message.service';
+import {validateEmail} from '../../utils/utils';
+
 function MessageBoard({setMessageList, messageList}) {
     let [email, setEmail] = useState('');
     let [message, setMessage] = useState('');
+    let [emailInvalid, setEmailInvalid] = useState(false);
+    let [messageInvalid, setMessageInvalid] = useState(false);
     let submitData = formData => {
-        postMessage(formData);
-        setMessage('');
-        setEmail('');
-        const email = formData.email, content = formData.message;
-        let newMessages = [{email, content}, ...messageList];
-        setMessageList(newMessages);
+        let isFormValid = validateForm(formData);
+        if(isFormValid){
+            postMessage(formData);
+            setMessage('');
+            setEmail('');
+            const email = formData.email, content = formData.message;
+            let newMessages = [{email, content}, ...messageList];
+            setMessageList(newMessages);
+        }
     }
+
+    let validateForm = formData => {
+        console.log('Validate form ', formData);
+        let isEmailInValid = !validateEmail(formData?.email);
+        let isMessageEmpty;
+        setEmailInvalid(isEmailInValid);
+
+        if(!(formData?.message)){
+            isMessageEmpty = true;
+            setMessageInvalid(true);
+        }
+        return !(isEmailInValid || isMessageEmpty);
+    };
+
+
+
     return (
         <div className={['container', styles.MessageBoard].join(' ')}>
             <div className="row justify-content-center">
@@ -21,13 +44,17 @@ function MessageBoard({setMessageList, messageList}) {
                 <form className={'fullWidth'}>
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" placeholder="name@example.com"
-                            value={email} onChange={e => setEmail(e.target.value)}/>
+                        <input type="email" className={["form-control", emailInvalid ? "is-invalid" : "" ].join(" ")} placeholder="name@example.com"
+                            value={email} onChange={e => setEmail(e.target.value)}
+                               onClick={() => setEmailInvalid(false)}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleFormControlTextarea1">Your Messages</label>
-                        <textarea className="form-control" rows="3" maxLength={500}
-                            value={message} onChange={e => setMessage(e.target.value)}/>
+                        <textarea className={["form-control", messageInvalid ? "is-invalid" :""].join(' ')} rows="3" maxLength={500}
+                            value={message} onChange={e => setMessage(e.target.value)}
+                             onClick={() => setMessageInvalid(false)}
+                        />
                     </div>
                     <div className={styles.actionButtons}>
                         <button type="button" className="btn btn-primary"
