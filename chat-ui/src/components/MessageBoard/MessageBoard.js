3cea4deb804a8f46c4b1846fd002e231b23d6649
Mohit Kumar <mkmohities7@gmail.com>
2021-01-24 17:18:37 +0000

    Message Board HTML completed

--- chat-ui/src/components/MessageBoard/MessageBoard.js
+++ chat-ui/src/components/MessageBoard/MessageBoard.js
@@ -1,9 +1,24 @@
 import styles from './MessageBoard.module.css';
 function MessageBoard() {
     return (
-        <div className="container">
+        <div className={['container', styles.MessageBoard].join(' ')}>
             <div className="row justify-content-center">
                 <h4>Message Board</h4>
+            </div>
+            <div className={'row'}>
+                <form className={styles.fullWidth}>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" placeholder="name@example.com" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlTextarea1">Your Messages</label>
+                        <textarea className="form-control" rows="3"></textarea>
+                    </div>
+                    <div className={styles.actionButtons}>
+                        <button type="submit" className="btn btn-primary">Post</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
